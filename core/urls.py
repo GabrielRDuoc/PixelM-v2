2d00c11bee32fd8f2790a716f2d95dcd376174f3
GabrielRDuoc <ga.rojase@duocuc.cl>
2023-04-13 00:07:09 -0400

    se corrige el index

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 #para agregar nuevas path de url hay que agregar una coma al diccionario urlpatterns y seguir el formato descrito
 urlpatterns ={
-    path('', index,name="index"),
-    
+    path('', index,name="index")
+     
 }
--- core/views.py
+++ core/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 # Create your views here. aca se debe crear una nueva funcion para la nueva landig
 def index(request):
     return render(request, 'core/index.html')
+
