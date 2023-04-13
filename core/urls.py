eb1645f3096a9bc1dd23ebcde63c7c0b450afaae
sebastianroja <sebastian.android.norambuena@gmail.com>
2023-04-12 23:55:32 -0400

    corregir navegacion

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 #para agregar nuevas path de url hay que agregar una coma al diccionario urlpatterns y seguir el formato descrito
 urlpatterns ={
-    path('index/', index,name="index"),
-    path('iniciarsesion/', iniciarsesion,name="iniciarsesion"),
-    path('contact/', contacto ,name="contacto"),
+    path('', index,name="index"),
+    
 }
