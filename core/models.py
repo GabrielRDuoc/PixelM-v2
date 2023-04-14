843a4984a2e006b60c105d6f46da6a5030f009f7
sebastianroja <sebastian.android.norambuena@gmail.com>
2023-04-14 17:59:42 -0400

    conexion bd

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='id de Categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la categoria')
+
+    
+
+    def __str__(self):
+        return self.nombreCategoria
